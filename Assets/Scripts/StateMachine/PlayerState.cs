3ace9d502b58f24f410962ceb34e476a0e198a25
Pulo-Lu <1286794316@qq,com>
2021-12-24 23:58:11 +0800

    提供基本状态方法

--- Assets/Scripts/StateMachine/PlayerState.cs
+++ Assets/Scripts/StateMachine/PlayerState.cs
@@ -4,8 +4,75 @@ using UnityEngine;
 
 public class PlayerState
 {
+    /// <summary>
+    /// 玩家脚本
+    /// </summary>
     protected Player player;
+    /// <summary>
+    /// 玩家数据脚本
+    /// </summary>
     protected PlayerData playerData;
+    /// <summary>
+    /// 状态机
+    /// </summary>
     protected StateMachine stateMachine;
+    /// <summary>
+    /// 动画切换名
+    /// </summary>
     protected string animBoolName;
+
+    /// <summary>
+    /// 构造方法
+    /// </summary>
+    /// <param name="player"></param>
+    /// <param name="playerData"></param>
+    /// <param name="stateMachine"></param>
+    /// <param name="animBoolName"></param>
+    public PlayerState(Player player, PlayerData playerData,StateMachine stateMachine, string animBoolName)
+    {
+        this.player = player;
+        this.playerData = playerData;
+        this.stateMachine = stateMachine;
+        this.animBoolName = animBoolName;
+    }
+
+    /// <summary>
+    /// 进入状态
+    /// </summary>
+    public virtual void Enter()
+    {
+
+    }
+
+    /// <summary>
+    /// 逻辑更新
+    /// </summary>
+    public virtual void LogicUpdate()
+    {
+
+    }
+
+    /// <summary>
+    /// 物理系统更新
+    /// </summary>
+    public virtual void PhysicsUpdate()
+    {
+        OnCheck();
+    }
+
+    /// <summary>
+    /// 退出状态
+    /// </summary>
+    public virtual void Exit()
+    {
+
+    }
+
+    /// <summary>
+    /// 射线检测
+    /// </summary>
+    public virtual void OnCheck()
+    {
+
+    }
 }
