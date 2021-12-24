6feb0d62c7cf475d1bc6e6c63960b46ce4271a90
Pulo-Lu <1286794316@qq,com>
2021-12-25 00:29:33 +0800

    玩家数据

--- Assets/Scripts/Data/PlayerData.cs
+++ Assets/Scripts/Data/PlayerData.cs
@@ -2,8 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 玩家数据脚本
+/// </summary>
 public class PlayerData : MonoBehaviour
 {
+    #region State
+    /// <summary>
+    /// 状态机
+    /// </summary>
+    public StateMachine stateMachine { get; private set; }
+
+
+    #endregion
+
+    private void Awake()
+    {
+        stateMachine = new StateMachine();
+    }
     // Start is called before the first frame update
     void Start()
     {
