d46370b2a9eec2e4adb6d17e5280bd4e0b29c348
Pulo-Lu <1286794316@qq,com>
2021-12-26 07:56:48 +0800

    在进入跳跃状态时，玩家跳跃的Bool设置为false；增加二段跳

--- Assets/Scripts/StateMachine/State/ChildState/PlayerJumpState.cs
+++ Assets/Scripts/StateMachine/State/ChildState/PlayerJumpState.cs
@@ -7,6 +7,12 @@ using UnityEngine;
 /// </summary>
 public class PlayerJumpState : PlayerAbiilityState
 {
+
+    /// <summary>
+    /// 跳跃次数计数器
+    /// </summary>
+    private int jumpCounter;
+
     /// <summary>
     /// 构造方法
     /// </summary>
@@ -23,11 +29,44 @@ public class PlayerJumpState : PlayerAbiilityState
     /// </summary>
     public override void Enter()
     {
+ 
         base.Enter();
+        //跳跃键已经按下，设置false
+        player.inputHandler.JumpInput = false;
         //设置竖直移动速度
         player.SetVelocityY(playerData.jumpVelocity);
         //切换能力行为
         isAbilityDone = true;
+        //跳跃次数减一
+        jumpCounter--;
+    }
+
+    /// <summary>
+    /// 能否跳跃
+    /// </summary>
+    /// <returns></returns>
+    public bool CanJump()
+    {
+        //跳跃次数大于0
+        return jumpCounter > 0;
+    }
+
+    /// <summary>
+    /// 重置跳跃次数
+    /// </summary>
+    public void ResetJumpCount()
+    {
+        //跳跃次数设置为最大跳跃次数
+        jumpCounter = playerData.maxJumpCount;
+    }
+
+    /// <summary>
+    /// 跳跃次数设置为0
+    /// </summary>
+    public void SetJumpCountZero()
+    {
+        //跳跃次数为0
+        jumpCounter = 0;
     }
 
 }
