cb6a4b8d55d8db057b000ba6523e0a6d112556a4
Pulo-Lu <1286794316@qq,com>
2021-12-25 11:38:41 +0800

    添加跳跃，检测半径数据

--- Assets/Scripts/Data/PlayerData.cs
+++ Assets/Scripts/Data/PlayerData.cs
@@ -9,6 +9,16 @@ using UnityEngine;
 public class PlayerData : ScriptableObject
 {
     [Header("Move State")]
-    ///水平方向移动速度
+    //水平方向移动速度
     public float movementVelocity = 10;
+
+    [Header("Jump State")]
+    //竖直方向移动速度
+    public float jumpVelocity = 20;
+
+    [Header("Check Data")]
+    //地面球形检测半径
+    public float GroundCheckRadius = 0.1f;
+    //地面检测层级
+    public LayerMask GroundLayer;
 }
